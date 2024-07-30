8c31fe1d175c0d14f2c073644541c733cafb45c0
Mireia Martin <mireia.martin07@gmail.com>
2024-07-30 11:28:00 +0200

    ex3 nivell1

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 import { useState } from 'react'
-import Card from './components/Card';
+import CardSteps from './components/Card';
+import meditation from './assets/meditation.svg';
+import programming from './assets/programming.svg';
+import time from './assets/time_managment.svg';
+
 
 function App() {
   const [step, setStep] = useState(0);
@@ -13,30 +17,31 @@ function App() {
   const tutorialData = [{
     title: 'Dedica moltes hores',
     description: 'Un mínim de 30 hores a la setmana. Si no en tens prou, hauràs de dedicar-li més hores. Al principi sembla impossible, però notaràs una millora ràpidament.',
-    bgColor: "white",
-    image: "image"
+    bgColor: '#63a0a8',
+    image: time
   },
   {
     title: 'Programa projectes propis',
     description: 'Més val 10 hores treballant en projectes propis, que he 10 hores mirant tutorials. La motivació i la implicació en el projecte ajudarà a accelerar el teu aprenentatge',
-    bgColor: "white",
-    image: "image"
+    bgColor: "#d3d4d9",
+    image: programming
   },
   {
     title: 'Procura descansar',
     description: "Descansar bé i desconnectar són vitals. D'aquesta manera reduiràs l'estrès i l'ansietat. Milloraràs la teva concentració i consolidaràs el teu aprenentatge.",
-    bgColor: "white",
-    image: "image"
+    bgColor: "#f9d278",
+    image: meditation
   }];
 
   return (
-    <>
-      <Card 
-        step = {step}
-        tutorialData = {tutorialData}
-        nextStep = {nextStep}
-      />
-    </>
+    <div className="container">
+    <CardSteps 
+      step={step}
+      tutorialData={tutorialData}
+      nextStep={nextStep}
+    />
+  </div>
+  
   )
 }
 
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,16 +1,28 @@
-const Card = ({step, tutorialData, nextStep}) => {
-    return (
-      <>
-        <img src={tutorialData[step].image} alt={tutorialData[step].title} className="card-image"/>
-        <div className="card-content">
-          <h2 className="card-title">{tutorialData[step].title}</h2>
-          <p className="card-description">{tutorialData[step].description}</p>
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import './card.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+const CardSteps = ({ step, tutorialData, nextStep }) => {
+  const bgColor = tutorialData[step].bgColor;
+  return (
+    <div className='container d-flex justify-content-center align-items-center min-vh-100'>
+      <Card className='card-content border-0'>
+        <div className='img-content' style={{ backgroundColor: bgColor }}>
+          <Card.Img className='card-img pt-2 mt-5' src={tutorialData[step].image} alt={tutorialData[step].title} />
         </div>
-        <div>
-        <button onClick={nextStep}>Next</button>
-      </div>
-      </>
-    )
-  };
-  
-export default Card;
+        <Card.Body className='card-body'>
+          <Card.Title className='card-title my-2 mx-1'>{tutorialData[step].title}</Card.Title>
+          <Card.Text className='card-text mx-1 text-black-50'>{tutorialData[step].description}</Card.Text>
+          <Button className='next-button mb-3 mx-2' variant="dark" onClick={nextStep}><i className="fas fa-arrow-right"></i></Button>
+        </Card.Body>
+      </Card>
+    </div>
+  );
+};
+
+
+
+export default CardSteps;
